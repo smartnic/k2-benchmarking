1e834dfec8a88005af24fa546f8aef4581d96b55
Nima Fallah <nfallah399@gmail.com>
2024-07-14 17:03:26 -0400

    Fixed incomplete command in 'run.sh'

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ for version in "${versions[@]}"; do
                         continue
                 fi
 
-                k2_cmd="../superopt/main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 1 --w_e 0.>
+		k2_cmd="../superopt/main_ebpf.out --bm_from_file --desc ${bm_file}.desc --bytecode ${bm_file}.insns --map ${bm_file}.maps -k 1 --is_win --port 8000 --logger_level 1 --w_e 0.5 --w_p 1.5 --st_ex 0 --st_eq 0 --st_avg 0 --st_perf 0 --st_when_to_restart 0 --st_when_to_restart_niter 0 --st_start_prog 0 --p_inst_operand 0.33333333 --p_inst 0.33333333 --p_inst_as_nop 0.15 --reset_win_niter 5000 --win_s_list 1 --win_e_list 9 --path_res $output_dir -n 25000"
                 $k2_cmd > "${output_dir}-log.txt"
         done
 done
